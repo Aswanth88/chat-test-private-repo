f4b004b89f82c70d6d1864fc53df0af1344fffe7
Revilla Aswanth <124183149+Aswanth88@users.noreply.github.com>
2023-09-06 00:33:31 +0530

    Update supabase.js

--- utils/supabase.js
+++ utils/supabase.js
@@ -2,6 +2,9 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Supabase URL and API key are required.');
+}
 const storage = typeof window !== 'undefined' ? window.localStorage : null;
 const supabase = createClient(
     supabaseUrl,
