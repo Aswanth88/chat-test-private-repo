643206cee6994c48702896018f4d4922d0f6e88c
Revilla Aswanth <124183149+Aswanth88@users.noreply.github.com>
2023-09-06 09:38:37 +0530

    Update supabase.js

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://hkjshlrvjeypvpvlevku.supabase.co'; 
-const supabaseKey = process.env.SUPABASE_KEY; 
+const supabaseUrl : 'https://hkjshlrvjeypvpvlevku.supabase.co'; 
+const supabaseKey : process.env.SUPABASE_KEY; 
 
 const storage = typeof window !== 'undefined' ? window.localStorage : null;
 const supabase = createClient(
