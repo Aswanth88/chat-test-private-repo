7f62cb5cb70e9cda84690aea574c1de40a2a068c
Revilla Aswanth <124183149+Aswanth88@users.noreply.github.com>
2023-09-07 07:41:44 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
         auth: {
           persistSession: false // or true
         },
-        SUPABASE_URL: process.env.SUPABASE_URL, // Include SUPABASE_URL
-        SUPABASE_KEY: process.env.SUPABASE_KEY, // Include SUPABASE_KEY
+        SUPABASE_URL: 'https://hkjshlrvjeypvpvlevku.supabase.co', // Include SUPABASE_URL
+        SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhranNobHJ2amV5cHZwdmxldmt1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MjcyNDQ1OCwiZXhwIjoyMDA4MzAwNDU4fQ.-_bEcqUS7jLJfiORT7xM8Lpkfg-ZXib3es6q3Q3NRYA', // Include SUPABASE_KEY
       }
     }
   },
